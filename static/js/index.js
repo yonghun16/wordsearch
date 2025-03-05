dc78b4819b188dce3cf069ea7d54cbd8b6d82132
yonghun16 <yonghun16@naver.com>
2025-03-06 08:32:06 +0900

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,28 @@
 import "./main.js";
 import "./footer.js";
 
+const newGame = document.querySelector(".new");
+
+
 async function createWord() {
   const res = await fetch("/create")
   const jsonRes = await res.json();
   console.log(jsonRes);
+
+  const rows = 12;
+  const cols = 12;
+  for (let i = 0; i < rows; i++) {
+    const row = document.querySelector(`.row-${i}`);
+    for (let j = 0; j < cols; j++) {
+      const column = row.querySelector(`[data-index="${i}${j}"]`);
+      column.innerHTML = jsonRes[i][j];
+    }
+  }
+
 }
 
-createWord();
+newGame.addEventListener("click", createWord)
+
+
 
 
