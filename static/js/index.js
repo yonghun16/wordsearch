5ef02c59cf2c4756799591854d7509319a8bc7ec
yonghun16 <yonghun16@naver.com>
2025-03-08 05:13:28 +0900

    structure reform

--- static/js/index.js
+++ static/js/index.js
@@ -1,61 +1,19 @@
-import "./main.js";
+import { handleCreateWord } from "./modules/newGame.js";
+import { createGridLine } from"./modules/gridLine.js";
 import "./footer.js";
 
+
 /*--- elements ---*/
+const [rows, cols] = [12, 12];
 const newGame = document.querySelector(".new");
 
-/*--- functions ---*/
-// 빈 격자에 랜덤한 알파벳 채우기
-function fillBlinkGrid(word_search_grid_word, rows, cols) {
-  for (let i = 0; i < rows; i++) {
-    const rowBlank = document.querySelector(`.row-${i}`);
-    for (let j = 0; j < cols; j++) {
-      const columnBlank = rowBlank.querySelector(`[data-index="${i}${j}"]`);
-      if (word_search_grid_word[i][j] === "_") {
-        word_search_grid_word[i][j] = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-        columnBlank.style.backgroundColor = "#FEFFFC";
-      }
-    }
-  }
-}
-// 정답 격자에 배경색 채우기
-function fillAnswerGrid(word_search_grid_num, rows, cols) {
-  const colorArray = ["#F9A19A", "#79C5BE", "#B39CDB", "#90CAF9", "#9FA8DA", "#BCAAA3", "#FFCB7F", "#AFBEC5", "#E6DF94", "#C6E1A4"];
-  for (let i = 0; i < rows; i++) {
-    const rowAnswer = document.querySelector(`.row-${i}`);
-    for (let j = 0; j < cols; j++) {
-      const columnAnswer = rowAnswer.querySelector(`[data-index="${i}${j}"]`);
-      if (word_search_grid_num[i][j] !== 0) {
-        columnAnswer.style.backgroundColor = `${colorArray[word_search_grid_num[i][j] - 1]}`;
-      }
-    }
-  }
-}
 
 /*--- functions ---*/
-// 격자판 생성
-async function handleCreateWord() {
-  const res = await fetch("/create")
-  const jsonRes = await res.json();
-  const rows = 12;
-  const cols = 12;
-  console.log(jsonRes)
-  const [word_search_grid_word, word_search_grid_num] = jsonRes;
+// 격자 생성
+createGridLine(rows, cols);
 
-  console.log(word_search_grid_word) 
-  console.log(word_search_grid_num) 
 
-  fillBlinkGrid(word_search_grid_word, rows, cols);
-  fillAnswerGrid(word_search_grid_num, rows, cols);
+/*--- events ---*/
+// 게임 생성
+newGame.addEventListener("click", () => handleCreateWord(rows, cols));
 
-  for (let i = 0; i < rows; i++) {
-    const row = document.querySelector(`.row-${i}`);
-    for (let j = 0; j < cols; j++) {
-      const column = row.querySelector(`[data-index="${i}${j}"]`);
-      column.innerHTML = word_search_grid_word[i][j];
-    }
-  }
-}
-
-/*--- event listeners ---*/
-newGame.addEventListener("click", handleCreateWord)
---
+++ static/js/modules/newGame.js
@@ -0,0 +1,49 @@
+/*--- functions ---*/
+// 빈 격자에 랜덤한 알파벳 채우기
+function fillBlinkGridWord(word_search_grid_word, rows, cols) {
+  for (let i = 0; i < rows; i++) {
+    const rowBlank = document.querySelector(`.row-${i}`);
+    for (let j = 0; j < cols; j++) {
+      const columnBlank = rowBlank.querySelector(`[data-index="${i}${j}"]`);
+      if (word_search_grid_word[i][j] === "_") {
+        word_search_grid_word[i][j] = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        columnBlank.style.backgroundColor = "#FEFFFC";
+      }
+    }
+  }
+}
+
+// 정답 격자에 배경색 채우기
+function fillAnswerGridColor(word_search_grid_num, rows, cols) {
+  const colorArray = ["#F9A19A", "#79C5BE", "#B39CDB", "#90CAF9", "#9FA8DA", "#BCAAA3", "#FFCB7F", "#AFBEC5", "#E6DF94", "#C6E1A4"];
+  for (let i = 0; i < rows; i++) {
+    const rowAnswer = document.querySelector(`.row-${i}`);
+    for (let j = 0; j < cols; j++) {
+      const columnAnswer = rowAnswer.querySelector(`[data-index="${i}${j}"]`);
+      if (word_search_grid_num[i][j] !== 0) {
+        columnAnswer.style.backgroundColor = `${colorArray[word_search_grid_num[i][j] - 1]}`;
+      }
+    }
+  }
+}
+
+/*--- handlers ---*/
+// 새로운 게임 생성
+export async function handleCreateWord(rows, cols) {
+  const res = await fetch("/create")
+  const jsonRes = await res.json();
+  const [word_search_grid_word, word_search_grid_num] = jsonRes;
+
+  fillBlinkGridWord(word_search_grid_word, rows, cols);
+  fillAnswerGridColor(word_search_grid_num, rows, cols);
+  
+  // 완성된 격자 HTML에 채우기
+  for (let i = 0; i < rows; i++) {
+    const row = document.querySelector(`.row-${i}`);
+    for (let j = 0; j < cols; j++) {
+      const column = row.querySelector(`[data-index="${i}${j}"]`);
+      column.innerHTML = word_search_grid_word[i][j];
+    }
+  }
+}
+
