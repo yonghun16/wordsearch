34345f4b347dd318d0593552497eca6dd7a2060f
yonghun16 <yonghun16@naver.com>
2025-03-08 04:07:07 +0900

    re

--- static/js/index.js
+++ static/js/index.js
@@ -6,27 +6,27 @@ const newGame = document.querySelector(".new");
 
 /*--- functions ---*/
 // 빈 격자에 랜덤한 알파벳 채우기
-//function fillBlinkGrid(jsonRes, rows, cols) {
-//  for (let i = 0; i < rows; i++) {
-//    const rowBlank = document.querySelector(`.row-${i}`);
-//    for (let j = 0; j < cols; j++) {
-//      const columnBlank = rowBlank.querySelector(`[data-index="${i}${j}"]`);
-//      if (jsonRes[i][j] === "_") {
-//        jsonRes[i][j] = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-//        columnBlank.style.backgroundColor = "#FEFFFC";
-//      }
-//    }
-//  }
-//}
+function fillBlinkGrid(word_search_grid_word, rows, cols) {
+  for (let i = 0; i < rows; i++) {
+    const rowBlank = document.querySelector(`.row-${i}`);
+    for (let j = 0; j < cols; j++) {
+      const columnBlank = rowBlank.querySelector(`[data-index="${i}${j}"]`);
+      if (word_search_grid_word[i][j] === "_") {
+        word_search_grid_word[i][j] = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        columnBlank.style.backgroundColor = "#FEFFFC";
+      }
+    }
+  }
+}
 // 정답 격자에 배경색 채우기
-function fillAnswerGrid(jsonRes, rows, cols) {
+function fillAnswerGrid(word_search_grid_num, rows, cols) {
   const colorArray = ["#F9A19A", "#79C5BE", "#B39CDB", "#90CAF9", "#9FA8DA", "#BCAAA3", "#FFCB7F", "#AFBEC5", "#E6DF94", "#C6E1A4"];
   for (let i = 0; i < rows; i++) {
     const rowAnswer = document.querySelector(`.row-${i}`);
     for (let j = 0; j < cols; j++) {
       const columnAnswer = rowAnswer.querySelector(`[data-index="${i}${j}"]`);
-      if (jsonRes[i][j] !== 0) {
-        columnAnswer.style.backgroundColor = `${colorArray[jsonRes[i][j] - 1]}`;
+      if (word_search_grid_num[i][j] !== 0) {
+        columnAnswer.style.backgroundColor = `${colorArray[word_search_grid_num[i][j] - 1]}`;
       }
     }
   }
@@ -37,18 +37,19 @@ function fillAnswerGrid(jsonRes, rows, cols) {
 async function handleCreateWord() {
   const res = await fetch("/create")
   const jsonRes = await res.json();
-  //const word_search_grid_num = jsonRes[1];
   const rows = 12;
   const cols = 12;
+  let word_search_grid_word = jsonRes[0];
+  const word_search_grid_num = jsonRes[1];
 
-  //fillBlinkGrid(jsonRes, rows, cols);
-  //fillAnswerGrid(word_search_grid_num, rows, cols);
+  fillBlinkGrid(word_search_grid_word, rows, cols);
+  fillAnswerGrid(word_search_grid_num, rows, cols);
 
   for (let i = 0; i < rows; i++) {
     const row = document.querySelector(`.row-${i}`);
     for (let j = 0; j < cols; j++) {
       const column = row.querySelector(`[data-index="${i}${j}"]`);
-      column.innerHTML = jsonRes[i][j];
+      column.innerHTML = word_search_grid_word[i][j];
     }
   }
 }
