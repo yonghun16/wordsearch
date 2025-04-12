9e670f6ae0a77855939bf648079c880cb9e0a667
yonghun16 <yonghun16@naver.com>
2025-04-12 15:28:55 +0900

    update

--- static/js/modules/sounds.js
+++ static/js/modules/sounds.js
@@ -1,5 +1,5 @@
-const blopSound = new Audio('../assets/blop.mp3');
-const correctSound = new Audio('../assets/correct.mp3');
-const applauseSound = new Audio('../assets/applause.mp3');
+const blopSound = new Audio('assets/blop.mp3');
+const correctSound = new Audio('assets/correct.mp3');
+const applauseSound = new Audio('assets/applause.mp3');
 
 export { blopSound, correctSound, applauseSound };
